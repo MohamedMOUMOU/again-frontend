6a2eb906a04cc21454d38d6329a62f7dc321e4eb
MohamedPro1 <41231930+MohamedPro1@users.noreply.github.com>
2020-06-01 17:06:39 +0100

    new version

--- src/views/examples/Deposit2.js
+++ src/views/examples/Deposit2.js
@@ -22,15 +22,15 @@ export default function Deposit() {
     <React.Fragment>
     <div className="container">
     <div className="row justify-content-center">
-      <span style={{ fontSize: 26,marginBottom: 20,marginTop: 10 } }><span style={ { color: '#ff6c31' } }>Add</span> another application</span> <br/>
-      <p>To submit a new application click the button bellow.</p>
+      <span style={{ fontSize: 26,marginBottom: 20,marginTop: 10 } }><span style={ { color: '#ff6c31' } }>Donate</span> another house</span> <br/>
+      <p>To donate another house click the button bellow.</p>
       <Button
         style={ { backgroundColor: '#ff6c31',color: 'white',fontWeight: 'bold' } }
         className="btn-round"
         color="success"
         size="lg"
         tag={Link}
-      >Add an application</Button>
+      >Donate a house</Button>
       </div>
       </div>
     </React.Fragment>
